95af8c54efccdbbd5f7e6bf39b8bb734c4cd638c
virgantara <vinux.edu@gmail.com>
2024-09-24 17:30:50 +0700

    + test.py

--- test.py
+++ test.py
@@ -18,7 +18,7 @@ from torchvision.models import resnet50, resnet18, resnet101
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
-from datasets.data import ShapeNetRender, ModelNet40SVM
+from datasets.data import ShapeNetRender, ModelNet40SVM, ScanObjectNNSVM
 from models.dgcnn import DGCNN_cls, DGCNN_partseg
 from models.pointnet import PointNet_cls, PointNet_partseg
 from models.resnet import ResNet
@@ -73,12 +73,22 @@ def parse_args():
                         help='Pretrained model path')
     return parser.parse_args()
 
-def train(args, io):
+def test(args, io):
     
     train_loader = DataLoader(ShapeNetRender(), num_workers=8,
                               batch_size=args.batch_size, shuffle=True, drop_last=True)
 
-    
+    #Try to load models
+    if args.model == 'dgcnn_cls':
+        point_model = DGCNN_cls(args).to(device)
+    elif args.model == 'dgcnn_seg':
+        point_model = DGCNN_partseg(args).to(device)
+    elif args.model == 'pointnet_cls':
+        point_model = PointNet_cls(args).to(device)
+    elif args.model == 'pointnet_seg':
+        point_model = PointNet_partseg(args).to(device)
+    else:
+        raise Exception("Not implemented")
         
     img_rgb_model = ResNet(resnet50(), feat_dim = 2048)
     img_rgb_model = img_rgb_model.to(device)
@@ -87,13 +97,15 @@ def train(args, io):
     img_gray_model = img_gray_model.to(device)
         
     
+    point_model.load_state_dict(torch.load(args.model_path))
+    print("Model Loaded !!")
         
-    parameters = list(img_rgb_model.parameters()) + list(img_gray_model.parameters())
+    parameters = list(point_model.parameters()) + list(img_rgb_model.parameters()) + list(img_gray_model.parameters())
     # print(parameters)
 
     # if args.use_sgd:
     print("Use SGD")
-    opt = optim.SGD(parameters, lr=args.lr*100, momentum=args.momentum, weight_decay=1e-4)
+    opt = optim.SGD(point_model.parameters(), lr=args.lr*100, momentum=args.momentum, weight_decay=1e-4)
     # else:
     # print("Use Adam")
     # opt = optim.Adam(parameters, lr=args.lr, weight_decay=1e-4)
@@ -103,67 +115,64 @@ def train(args, io):
     
     best_acc = 0
 
-    # Initialize weights and their rates of change
-    lambda_intra = 1.0
-    lambda_cross = 1.0
-    alpha = 0.05  # Control sensitivity of adjustment
-    beta = 0.95  # Smoothing factor
-
-    prev_loss_intra = 0
-    prev_loss_cross = 0
+    
     for epoch in range(args.start_epoch, args.epochs):
-        ####################
-        # Train
-        ####################
-        train_losses = AverageMeter()
-        train_intra_losses = AverageMeter()
-        train_cross_losses = AverageMeter()
         
-        
-        img_rgb_model.train()
-        img_gray_model.train()
-        wandb_log = {}
         print(f'Start training epoch: ({epoch}/{args.epochs})')
-        for (data_t1, data_t2), (img_rgb, img_gray) in tqdm(train_loader):
-            data_t1, data_t2, img_rgb, img_gray = data_t1.to(device), data_t2.to(device), img_rgb.to(device), img_gray.to(device)
-            # print('data_t1=>',data_t1.shape,'data_t2=>',data_t2.shape,'imgs=>',imgs.shape)
-            batch_size = data_t1.size()[0]
 
-            opt.zero_grad()
-            data = torch.cat((data_t1, data_t2))
-            data = data.transpose(2, 1).contiguous()
+        # Testing
+        train_val_loader = DataLoader(ScanObjectNNSVM(partition='train', num_points=1024), batch_size=64, shuffle=True)
+        test_val_loader = DataLoader(ScanObjectNNSVM(partition='test', num_points=1024), batch_size=64, shuffle=False)
+
+        feats_train = []
+        labels_train = []
+        point_model.eval()
+
+        for (data, label) in tqdm(train_val_loader):
+            # print('data=>',data.shape,'label=>',label.shape) #[B,num_points,3]
+            labels = list(map(lambda x: x[0],label.numpy().tolist()))
+            # print('labels=>',labels) #[B,1]的label标签转换为一个大小为B的数组
+            data = data.permute(0, 2, 1).to(device)
+            with torch.no_grad():
+                feats = point_model(data)[-1]
+            feats = feats.detach().cpu().numpy()
+            # print('feats=>', feats.shape) #[B,2048(max1024+avg1024)]
+            for feat in feats:
+                feats_train.append(feat)
+            labels_train += labels
+
+        feats_train = np.array(feats_train)
+        labels_train = np.array(labels_train)
+        # print('feats_train=>',feats_train.shape,'labels_train=>',labels_train.shape) #(9840, 2048),(9840,)
+
+        feats_test = []
+        labels_test = []
+
+        for data, label in tqdm(test_val_loader):
+            labels = list(map(lambda x: x[0],label.numpy().tolist()))
+            data = data.permute(0, 2, 1).to(device)
+            with torch.no_grad():
+                feats = point_model(data)[-1]
+            feats = feats.detach().cpu().numpy()
+            for feat in feats:
+                feats_test.append(feat)
+            labels_test += labels
+
+        feats_test = np.array(feats_test)
+        labels_test = np.array(labels_test)
+        
+        model_tl = SVC(C = 0.01, kernel ='linear')
+  
+        model_tl.fit(feats_train, labels_train)
+        # print('model_tl=>', model_tl)
+        test_accuracy = model_tl.score(feats_test, labels_test)
+        
+        io.cprint(f"Linear Accuracy : {test_accuracy}, Best Accuracy : {best_acc}")
+        
+        if test_accuracy > best_acc:
+            best_acc = test_accuracy
             
-            img_rgb_feats = img_rgb_model(img_rgb)
-            img_gray_feats = img_gray_model(img_gray)
-
-            image_feats = torch.cat([img_rgb_feats, img_gray_feats],dim = 0)
-
-            loss_cross = criterion(image_feats)
-
-            # Dynamic weight adjustment
-            delta_cross = loss_cross.item() - prev_loss_cross
-
-           
-            lambda_cross = beta * lambda_cross + (1 - beta) * (1 / (1 + np.exp(-alpha * delta_cross)))
-
-            # Update previous loss values
-            prev_loss_cross = loss_cross.item()
-
-            total_loss = lambda_cross * loss_cross
-            total_loss.backward()
-            opt.step()
-            lr_scheduler.step()
-
-            train_losses.update(total_loss.item(), batch_size)
-            train_intra_losses.update(loss_intra.item(), batch_size)
-            train_cross_losses.update(loss_cross.item(), batch_size)
-
-        outstr = 'Epoch (%d), Batch(%d), loss: %.6f, intra loss: %.6f, cross loss: %.6f' % \
-                 (epoch, len(train_loader), train_losses.avg, train_intra_losses.avg, train_cross_losses.avg)
-        io.cprint(outstr)
-
-      
-
+    
 
 if __name__ == "__main__":
     args = parse_args()
@@ -176,9 +185,6 @@ if __name__ == "__main__":
 
     torch.manual_seed(args.seed)
 
-    if not args.eval:
-        train(args, io)
-    else:
-        test(args, io)
+    test(args, io)
 
 
