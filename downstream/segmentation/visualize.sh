fe0204892c3c3539a57f9c1feb2f5e9475f11130
virgantara <vinux.edu@gmail.com>
2025-03-25 23:51:08 +0700

    edit vis

---
+++ downstream/segmentation/visualize.sh
@@ -0,0 +1,31 @@
+python downstream/segmentation/main_partseg.py --batch_size=32 --test_batch_size=24 --model=dgcnn --eval=True --model_path=checkpoints/exp_partseg_downstream_dyna_dgcnn_bs32_ep400_sgd/models/whole_model.pt --exp_name=exp_partseg_downstream_dyna_dgcnn_bs32_ep400_sgd --visu_format=txt --visu=airplane
+
+python downstream/segmentation/main_partseg.py --batch_size=32 --test_batch_size=24 --model=dgcnn --eval=True --model_path=checkpoints/exp_partseg_downstream_dyna_dgcnn_bs32_ep400_sgd/models/whole_model.pt --exp_name=exp_partseg_downstream_dyna_dgcnn_bs32_ep400_sgd --visu_format=txt --visu=bag
+
+python downstream/segmentation/main_partseg.py --batch_size=32 --test_batch_size=24 --model=dgcnn --eval=True --model_path=checkpoints/exp_partseg_downstream_dyna_dgcnn_bs32_ep400_sgd/models/whole_model.pt --exp_name=exp_partseg_downstream_dyna_dgcnn_bs32_ep400_sgd --visu_format=txt --visu=cap
+
+python downstream/segmentation/main_partseg.py --batch_size=32 --test_batch_size=24 --model=dgcnn --eval=True --model_path=checkpoints/exp_partseg_downstream_dyna_dgcnn_bs32_ep400_sgd/models/whole_model.pt --exp_name=exp_partseg_downstream_dyna_dgcnn_bs32_ep400_sgd --visu_format=txt --visu=car
+
+python downstream/segmentation/main_partseg.py --batch_size=32 --test_batch_size=24 --model=dgcnn --eval=True --model_path=checkpoints/exp_partseg_downstream_dyna_dgcnn_bs32_ep400_sgd/models/whole_model.pt --exp_name=exp_partseg_downstream_dyna_dgcnn_bs32_ep400_sgd --visu_format=txt --visu=chair
+
+python downstream/segmentation/main_partseg.py --batch_size=32 --test_batch_size=24 --model=dgcnn --eval=True --model_path=checkpoints/exp_partseg_downstream_dyna_dgcnn_bs32_ep400_sgd/models/whole_model.pt --exp_name=exp_partseg_downstream_dyna_dgcnn_bs32_ep400_sgd --visu_format=txt --visu=earphone
+
+python downstream/segmentation/main_partseg.py --batch_size=32 --test_batch_size=24 --model=dgcnn --eval=True --model_path=checkpoints/exp_partseg_downstream_dyna_dgcnn_bs32_ep400_sgd/models/whole_model.pt --exp_name=exp_partseg_downstream_dyna_dgcnn_bs32_ep400_sgd --visu_format=txt --visu=guitar
+
+python downstream/segmentation/main_partseg.py --batch_size=32 --test_batch_size=24 --model=dgcnn --eval=True --model_path=checkpoints/exp_partseg_downstream_dyna_dgcnn_bs32_ep400_sgd/models/whole_model.pt --exp_name=exp_partseg_downstream_dyna_dgcnn_bs32_ep400_sgd --visu_format=txt --visu=knife
+
+python downstream/segmentation/main_partseg.py --batch_size=32 --test_batch_size=24 --model=dgcnn --eval=True --model_path=checkpoints/exp_partseg_downstream_dyna_dgcnn_bs32_ep400_sgd/models/whole_model.pt --exp_name=exp_partseg_downstream_dyna_dgcnn_bs32_ep400_sgd --visu_format=txt --visu=lamp
+
+python downstream/segmentation/main_partseg.py --batch_size=32 --test_batch_size=24 --model=dgcnn --eval=True --model_path=checkpoints/exp_partseg_downstream_dyna_dgcnn_bs32_ep400_sgd/models/whole_model.pt --exp_name=exp_partseg_downstream_dyna_dgcnn_bs32_ep400_sgd --visu_format=txt --visu=laptop
+
+python downstream/segmentation/main_partseg.py --batch_size=32 --test_batch_size=24 --model=dgcnn --eval=True --model_path=checkpoints/exp_partseg_downstream_dyna_dgcnn_bs32_ep400_sgd/models/whole_model.pt --exp_name=exp_partseg_downstream_dyna_dgcnn_bs32_ep400_sgd --visu_format=txt --visu=motor
+
+python downstream/segmentation/main_partseg.py --batch_size=32 --test_batch_size=24 --model=dgcnn --eval=True --model_path=checkpoints/exp_partseg_downstream_dyna_dgcnn_bs32_ep400_sgd/models/whole_model.pt --exp_name=exp_partseg_downstream_dyna_dgcnn_bs32_ep400_sgd --visu_format=txt --visu=mug
+
+python downstream/segmentation/main_partseg.py --batch_size=32 --test_batch_size=24 --model=dgcnn --eval=True --model_path=checkpoints/exp_partseg_downstream_dyna_dgcnn_bs32_ep400_sgd/models/whole_model.pt --exp_name=exp_partseg_downstream_dyna_dgcnn_bs32_ep400_sgd --visu_format=txt --visu=pistol
+
+python downstream/segmentation/main_partseg.py --batch_size=32 --test_batch_size=24 --model=dgcnn --eval=True --model_path=checkpoints/exp_partseg_downstream_dyna_dgcnn_bs32_ep400_sgd/models/whole_model.pt --exp_name=exp_partseg_downstream_dyna_dgcnn_bs32_ep400_sgd --visu_format=txt --visu=rocket
+
+python downstream/segmentation/main_partseg.py --batch_size=32 --test_batch_size=24 --model=dgcnn --eval=True --model_path=checkpoints/exp_partseg_downstream_dyna_dgcnn_bs32_ep400_sgd/models/whole_model.pt --exp_name=exp_partseg_downstream_dyna_dgcnn_bs32_ep400_sgd --visu_format=txt --visu=skateboard
+
+python downstream/segmentation/main_partseg.py --batch_size=32 --test_batch_size=24 --model=dgcnn --eval=True --model_path=checkpoints/exp_partseg_downstream_dyna_dgcnn_bs32_ep400_sgd/models/whole_model.pt --exp_name=exp_partseg_downstream_dyna_dgcnn_bs32_ep400_sgd --visu_format=txt --visu=table 
---
+++ visualize_crossnet.sh
@@ -0,0 +1,31 @@
+python downstream/segmentation/main_partseg.py --batch_size=32 --test_batch_size=24 --model=dgcnn --eval=True --model_path=/home/deep/OddyProjects/DynamicCrossNet/checkpoints/exp_partseg_downstream_dyna_dgcnn_bs32_ep400_sgd/models/whole_model.pt --exp_name=exp_partseg_adacrossnet  --visu=airplane
+
+python downstream/segmentation/main_partseg.py --batch_size=32 --test_batch_size=24 --model=dgcnn --eval=True --model_path=/home/deep/OddyProjects/DynamicCrossNet/checkpoints/exp_partseg_downstream_dyna_dgcnn_bs32_ep400_sgd/models/whole_model.pt --exp_name=exp_partseg_adacrossnet  --visu=bag
+
+python downstream/segmentation/main_partseg.py --batch_size=32 --test_batch_size=24 --model=dgcnn --eval=True --model_path=/home/deep/OddyProjects/DynamicCrossNet/checkpoints/exp_partseg_downstream_dyna_dgcnn_bs32_ep400_sgd/models/whole_model.pt --exp_name=exp_partseg_adacrossnet  --visu=cap
+
+python downstream/segmentation/main_partseg.py --batch_size=32 --test_batch_size=24 --model=dgcnn --eval=True --model_path=/home/deep/OddyProjects/DynamicCrossNet/checkpoints/exp_partseg_downstream_dyna_dgcnn_bs32_ep400_sgd/models/whole_model.pt --exp_name=exp_partseg_adacrossnet  --visu=car
+
+python downstream/segmentation/main_partseg.py --batch_size=32 --test_batch_size=24 --model=dgcnn --eval=True --model_path=/home/deep/OddyProjects/DynamicCrossNet/checkpoints/exp_partseg_downstream_dyna_dgcnn_bs32_ep400_sgd/models/whole_model.pt --exp_name=exp_partseg_adacrossnet  --visu=chair
+
+python downstream/segmentation/main_partseg.py --batch_size=32 --test_batch_size=24 --model=dgcnn --eval=True --model_path=/home/deep/OddyProjects/DynamicCrossNet/checkpoints/exp_partseg_downstream_dyna_dgcnn_bs32_ep400_sgd/models/whole_model.pt --exp_name=exp_partseg_adacrossnet  --visu=earphone
+
+python downstream/segmentation/main_partseg.py --batch_size=32 --test_batch_size=24 --model=dgcnn --eval=True --model_path=/home/deep/OddyProjects/DynamicCrossNet/checkpoints/exp_partseg_downstream_dyna_dgcnn_bs32_ep400_sgd/models/whole_model.pt --exp_name=exp_partseg_adacrossnet  --visu=guitar
+
+python downstream/segmentation/main_partseg.py --batch_size=32 --test_batch_size=24 --model=dgcnn --eval=True --model_path=/home/deep/OddyProjects/DynamicCrossNet/checkpoints/exp_partseg_downstream_dyna_dgcnn_bs32_ep400_sgd/models/whole_model.pt --exp_name=exp_partseg_adacrossnet  --visu=knife
+
+python downstream/segmentation/main_partseg.py --batch_size=32 --test_batch_size=24 --model=dgcnn --eval=True --model_path=/home/deep/OddyProjects/DynamicCrossNet/checkpoints/exp_partseg_downstream_dyna_dgcnn_bs32_ep400_sgd/models/whole_model.pt --exp_name=exp_partseg_adacrossnet  --visu=lamp
+
+python downstream/segmentation/main_partseg.py --batch_size=32 --test_batch_size=24 --model=dgcnn --eval=True --model_path=/home/deep/OddyProjects/DynamicCrossNet/checkpoints/exp_partseg_downstream_dyna_dgcnn_bs32_ep400_sgd/models/whole_model.pt --exp_name=exp_partseg_adacrossnet  --visu=laptop
+
+python downstream/segmentation/main_partseg.py --batch_size=32 --test_batch_size=24 --model=dgcnn --eval=True --model_path=/home/deep/OddyProjects/DynamicCrossNet/checkpoints/exp_partseg_downstream_dyna_dgcnn_bs32_ep400_sgd/models/whole_model.pt --exp_name=exp_partseg_adacrossnet  --visu=motor
+
+python downstream/segmentation/main_partseg.py --batch_size=32 --test_batch_size=24 --model=dgcnn --eval=True --model_path=/home/deep/OddyProjects/DynamicCrossNet/checkpoints/exp_partseg_downstream_dyna_dgcnn_bs32_ep400_sgd/models/whole_model.pt --exp_name=exp_partseg_adacrossnet  --visu=mug
+
+python downstream/segmentation/main_partseg.py --batch_size=32 --test_batch_size=24 --model=dgcnn --eval=True --model_path=/home/deep/OddyProjects/DynamicCrossNet/checkpoints/exp_partseg_downstream_dyna_dgcnn_bs32_ep400_sgd/models/whole_model.pt --exp_name=exp_partseg_adacrossnet  --visu=pistol
+
+python downstream/segmentation/main_partseg.py --batch_size=32 --test_batch_size=24 --model=dgcnn --eval=True --model_path=/home/deep/OddyProjects/DynamicCrossNet/checkpoints/exp_partseg_downstream_dyna_dgcnn_bs32_ep400_sgd/models/whole_model.pt --exp_name=exp_partseg_adacrossnet  --visu=rocket
+
+python downstream/segmentation/main_partseg.py --batch_size=32 --test_batch_size=24 --model=dgcnn --eval=True --model_path=/home/deep/OddyProjects/DynamicCrossNet/checkpoints/exp_partseg_downstream_dyna_dgcnn_bs32_ep400_sgd/models/whole_model.pt --exp_name=exp_partseg_adacrossnet  --visu=skateboard
+
+python downstream/segmentation/main_partseg.py --batch_size=32 --test_batch_size=24 --model=dgcnn --eval=True --model_path=/home/deep/OddyProjects/DynamicCrossNet/checkpoints/exp_partseg_downstream_dyna_dgcnn_bs32_ep400_sgd/models/whole_model.pt --exp_name=exp_partseg_adacrossnet  --visu=table 
